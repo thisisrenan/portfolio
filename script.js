f979d30678c68f885e49709b2a8472092ef3389a
thisisrenan <joserenandasilva08@gmail.com>
2024-03-03 04:05:04 -0300

    commit init

---
+++ script.js
@@ -0,0 +1,54 @@
+class DraggableDiv {
+    constructor(element) {
+        this.element = element;
+        this.isDragging = false;
+        this.offsetX = 0;
+        this.offsetY = 0;
+        this.maxZIndex = 999;
+
+        this.element.addEventListener("mousedown", (e) => this.startDrag(e));
+    }
+
+    startDrag(e) {
+        this.isDragging = true;
+        this.offsetX = e.clientX - this.element.getBoundingClientRect().left;
+        this.offsetY = e.clientY - this.element.getBoundingClientRect().top;
+        this.element.style.cursor = "grabbing";
+
+        // Incrementar o z-index máximo
+        this.maxZIndex += 1;
+        this.element.style.zIndex = this.maxZIndex;
+
+        // Mudar a posição para relative se for static
+        if (window.getComputedStyle(this.element).position === "static") {
+            this.element.style.position = "relative";
+        }
+
+        document.addEventListener("mousemove", (e) => this.drag(e));
+        document.addEventListener("mouseup", () => this.stopDrag());
+    }
+
+    drag(e) {
+        if (this.isDragging) {
+            const x = e.clientX - this.offsetX;
+            const y = e.clientY - this.offsetY;
+            this.element.style.left = `${x}px`;
+            this.element.style.top = `${y}px`;
+        }
+    }
+
+    stopDrag() {
+        this.isDragging = false;
+        this.element.style.cursor = "grab";
+        document.removeEventListener("mousemove", (e) => this.drag(e));
+        document.removeEventListener("mouseup", () => this.stopDrag());
+    }
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    const divs = document.querySelectorAll(".movable-div");
+
+    divs.forEach((div) => {
+        new DraggableDiv(div);
+    });
+});
